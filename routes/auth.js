7100de58108ccf197d7b722fbac2bb3822e60ad4
keshav4r3 <143509093+KESHAV4R3@users.noreply.github.com>
2025-06-18 13:08:52 +0530

    Update auth.js added google authnetication routes

--- routes/auth.js
+++ routes/auth.js
@@ -4,12 +4,14 @@ const express = require('express');
 const authRouter = express.Router();
 
 // import the controllers
-const { signUp, login, logout, loginAutomatic, sendOtp, sendOtpToOwner, signUpAdmin } = require('../controllers/auth');
+const { signUp, login, logout, loginAutomatic, sendOtp,googleLogin,googleSignUp, sendOtpToOwner, signUpAdmin } = require('../controllers/auth');
 
 authRouter.post('/logout', logout);
 authRouter.post('/automatic-login', loginAutomatic);
 authRouter.post('/sendOtp', sendOtp);
 authRouter.post('/signup', signUp);
+authRouter.post('/googleLogin', googleLogin);
+authRouter.post('/googleSignUp', googleSignUp);
 authRouter.post('/login', login);
 authRouter.post('/sendOtp_owner', sendOtpToOwner);
 authRouter.post('/signup_Admin', signUpAdmin);
